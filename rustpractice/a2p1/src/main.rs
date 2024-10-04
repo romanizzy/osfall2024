8353aa23de1dea4f1b53b69a5a0d537f9d5444b2
Izabella Valero <109047364+romanizzy@users.noreply.github.com>
2024-10-04 19:42:31 +0000

    Make part two of the first assignnment

---
+++ rustpractice/a2p1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    
+}
+
+
+//implement the function sum_with_step
+// takes a mutable reference for the sum result and three integers: low high and step
+// calculates the sum from low to high (inclusive) with the given step
+//stores the result in the mutable reference
---
+++ rustpractice/a2p2/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    
+}
+
+//create a program that:
+// takes a string of text as innput
+// splits the text into words (space as separator) 
+// text.split_whitespace().collect();
+// count the frequenncy of each word
+// returns the word with the highest frequency and its count
+
+// requirements 
+// use mutable references where appropriate
+// avoid using HashMaps or complex data structures.
